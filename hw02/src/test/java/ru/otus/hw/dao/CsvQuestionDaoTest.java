454f08f64cc0b1734c2c05d19fe447cb84fe8ece
Aleksey Balakin <Aleksey.Balakin@x5.ru>
2025-01-14 15:16:39 +0300

    Исправление после ревью HW02

--- hw02/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
+++ hw02/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
@@ -1,19 +1,16 @@
 package ru.otus.hw.dao;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import ru.otus.hw.config.TestFileNameProvider;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
-import static org.mockito.MockitoAnnotations.openMocks;
 
+@ExtendWith(MockitoExtension.class)
 class CsvQuestionDaoTest {
 
     @Mock
@@ -22,22 +19,11 @@ class CsvQuestionDaoTest {
     @InjectMocks
     private CsvQuestionDao csvQuestionDao;
 
-    private List<String[]> csvValues = new ArrayList<>();
-
-    private static final String FILE_NAME = "questions.csv";
-
-    @BeforeEach
-    void setUp() {
-        openMocks(this);
-
-        var inputStream = getClass().getClassLoader().getResourceAsStream(FILE_NAME);
-        var scanner = new Scanner(inputStream);
-        scanner.nextLine();
-        while(scanner.hasNextLine()) {
-            String[] values = scanner.nextLine().split(";");
-            csvValues.add(values);
-        }
-    }
+    private static final String FILE_NAME = "test_questions.csv";
+    private static final String QUESTION_1 = "Is there life on Mars?";
+    private static final String QUESTION_2 = "How should resources be loaded from jar in Java?";
+    private static final String QUESTION_3 = "Which option is a good way to handle the exception?";
+    private static final int QUESTION_COUNT = 3;
 
     @Test
     void findAll() {
@@ -45,9 +31,9 @@ class CsvQuestionDaoTest {
 
         var questionList = csvQuestionDao.findAll();
 
-        assertEquals(csvValues.size(), questionList.size());
-        assertEquals(csvValues.get(0)[0], questionList.get(0).text());
-        assertEquals(csvValues.get(1)[0], questionList.get(1).text());
-        assertEquals(csvValues.get(2)[0], questionList.get(2).text());
+        assertEquals(QUESTION_COUNT, questionList.size());
+        assertEquals(QUESTION_1, questionList.get(0).text());
+        assertEquals(QUESTION_2, questionList.get(1).text());
+        assertEquals(QUESTION_3, questionList.get(2).text());
     }
 }
--- hw02/src/test/java/ru/otus/hw/service/TestServiceImplTest.java
+++ hw02/src/test/java/ru/otus/hw/service/TestServiceImplTest.java
@@ -1,16 +1,23 @@
 package ru.otus.hw.service;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
 import ru.otus.hw.domain.Student;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.MockitoAnnotations.openMocks;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class TestServiceImplTest {
 
     @Mock
@@ -21,21 +28,41 @@ class TestServiceImplTest {
     @InjectMocks
     private TestServiceImpl testService;
 
-    @BeforeEach
-    void setUp() {
-        openMocks(this);
-    }
+    private static final String ANSWER_PROMPT = "Enter you answer (number): ";
+    private static final String ANSWER_ERROR = "Incorrect answer number! Please, try again.";
 
-    @Test
-    void executeTestFor() {
+    @ParameterizedTest
+    @CsvSource({"1, 1", "3, 0"})
+    void executeTestFor(int answerNumber, int rightAnswerCount) {
         var student = getStudent();
+        var questionList = getQuestions();
+        var answerList = questionList.get(0).answers();
+        when(questionDao.findAll()).thenReturn(questionList);
+        when(ioService.readIntForRangeWithPrompt(1, 3, ANSWER_PROMPT, ANSWER_ERROR)).thenReturn(answerNumber);
+
         var testResult = testService.executeTestFor(student);
 
+        verify(ioService).printLine("");
+        verify(ioService).printFormattedLine("Please answer the questions below%n");
+        verify(ioService).printFormattedLine("Question: %s", questionList.get(0).text());
+        verify(ioService).printFormattedLine("    Answer #%s: %s",  1,answerList.get(0).text());
+        verify(ioService).printFormattedLine("    Answer #%s: %s",  2,answerList.get(1).text());
+        verify(ioService).printFormattedLine("    Answer #%s: %s",  3,answerList.get(2).text());
+        verify(ioService).readIntForRangeWithPrompt(1, 3, ANSWER_PROMPT, ANSWER_ERROR);
+
         assertEquals(student, testResult.getStudent());
-        assertEquals(0, testResult.getRightAnswersCount());
+        assertEquals(rightAnswerCount, testResult.getRightAnswersCount());
     }
 
     private Student getStudent() {
         return new Student("Ivan", "Petrov");
+    }
+
+    private List<Question> getQuestions() {
+        var answer1 = new Answer("answer1", true);
+        var answer2 = new Answer("answer2", false);
+        var answer3 = new Answer("answer3", false);
+        var question = new Question("question1", List.of(answer1, answer2, answer3));
+        return List.of(question);
     }
 }
